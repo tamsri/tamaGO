cb18639fdcf4c86111729a85c2a8f027730fbfd8
Supawat Tamsri <meep@supawat.dev>
2021-07-18 20:32:14 +0200

    initailize

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/tamsri/tamaGO
+
+go 1.16
+
+require rsc.io/quote v1.5.2
---
+++ index.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	"rsc.io/quote"
+)
+
+func main() {
+	fmt.Println("Begin TamaGO")
+	fmt.Println(quote.Go())
+	fmt.Println("End TamaGO")
+}
